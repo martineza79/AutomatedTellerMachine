88bd6f9d192dd3ec1e0370fa2b784ac05c622615
ALEX MARTINEZ <martineza79@outlook.com>
2017-01-08 16:53:04 -0600

    Add project files.

---
+++ AutomatedTellerMachine.Tests/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutomatedTellerMachine.Controllers;
+using System.Web.Mvc;
+using AutomatedTellerMachine.Models;
+
+namespace AutomatedTellerMachine.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FooActionReturnsAboutView()
+        {
+            //Arrange
+            var homeController = new HomeController();
+            //Act
+            var result = homeController.Foo() as ViewResult;
+            //Assert
+            Assert.AreEqual("About", result.ViewName);
+        }
+
+        [TestMethod]
+        public void ContactFormSaysThanks()
+        {
+            //Arrange
+            var homeController = new HomeController();
+            //Act
+            //***MUST CHANGE TO "PartialViewResult" BLOW TO ALLOW AJAX TO WORK ON APP***//
+            var result = homeController.Contact("I love your bank.") as PartialViewResult;
+            // ***USE THE "as ViewResult" IN ACT STATEMENT TO ACCESS THE "ViewBag" IN ASSERT STATEMENT BELOW***//
+            //Assert
+            Assert.IsNotNull(result.ViewBag.TheMessage);
+        }
+
+        [TestMethod]
+        public void BalanceIsCorrectAfterDeposit()
+        {
+            //Arrange
+            var fakeDb = new FakeApplicationDbContext();
+            fakeDb.CheckingAccounts = new FakeDbSet<CheckingAccount>();
+
+            var checkingAccount = new CheckingAccount { Id = 1, AccountNumber = "000123TEST", Balance = 0 };
+            fakeDb.CheckingAccounts.Add(checkingAccount);
+            fakeDb.Transactions = new FakeDbSet<Transaction>();
+            var transactionController = new TransactionController(fakeDb);
+
+            //Act
+            transactionController.Deposit(new Transaction { CheckingAccountId = 1, Amount = 25 });
+
+            //Assert
+            Assert.AreEqual(25, checkingAccount.Balance);
+        }
+    }
+}
---
+++ AutomatedTellerMachine/Migrations/Configuration.cs
@@ -0,0 +1,71 @@
+namespace AutomatedTellerMachine.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using Services;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AutomatedTellerMachine.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "AutomatedTellerMachine.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(AutomatedTellerMachine.Models.ApplicationDbContext context)
+        {
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            if (!context.Users.Any(t=>t.UserName == "admin@mvcatm.com"))
+            {
+                var user = new ApplicationUser { UserName = "admin@mvcatm.com", Email = "admin@mvcatm.com" };
+                IdentityResult result = userManager.Create(user, "passW0rd!");
+
+                var service = new CheckingAccountService(context);
+                service.CreateCheckingAccount("admin", "user", user.Id, 1000);
+
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Admin" });
+                context.SaveChanges();
+
+                userManager.AddToRole(user.Id, "Admin");
+                context.SaveChanges();
+            }
+
+            //context.Transactions.Add(new Transaction { Amount = 75, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = -25, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 100000, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 19.99m, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 64.40m, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 100, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = -300, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 255.75m, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 198, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 2, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 50, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = -1.50m, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 6100, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = 164.84m, CheckingAccountId = 5 });
+            //context.Transactions.Add(new Transaction { Amount = .01m, CheckingAccountId = 5 });
+
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
